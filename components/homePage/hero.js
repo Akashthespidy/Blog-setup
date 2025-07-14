fa4b82698523b6c1e60d760be721e582350a45d8
Billal <akashjnu26@gmail.com>
2025-07-14 11:57:26 +0600

    home page image is added

--- components/homePage/hero.js
+++ components/homePage/hero.js
@@ -1,8 +1,17 @@
 import classes from "./hero.module.css";
+import Image from "next/image";
 export default function Hero() {
   return (
-    <section>
-      <div> Image</div>
+    <section className="${classes.hero}">
+      <div className={classes.image}>
+        {" "}
+        <Image
+          src="/images/site/Akash.jpg"
+          alt="An Image showing Akash"
+          width={300}
+          height={300}
+        />
+      </div>
       <h1>👋 Hi, I am Billal Ahmed Akash</h1>
       <p>
         💡 I blog about problem solving, programming 💻, and web development 🌐
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import { Fragment } from "react/jsx-runtime";
-
+import Hero from "../components/homePage/hero";
 export default function Home() {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts />
     </Fragment>
   );
 }
